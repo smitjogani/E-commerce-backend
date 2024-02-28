95a82df46dac82fc71d69daaacb1f753cadb5828
smit <smitjogani00@gmail.com>
2024-02-28 08:25:47 +0530

    Backaend

---
+++ api/Services/Cart/cart.service.js
@@ -0,0 +1,12 @@
+const Cart = require("../../models/Cart/cart.model");
+
+async function createCart(user) {
+  try {
+    const cart = new Cart({ user });
+    const createdCart = await cart.save();
+    return createdCart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+module.exports = { createCart };
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const authRouters = require("./api/routes/auth/auth.routes");
+const userRouters = require("./api/routes/user/user.routes");
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", authRouters);
+app.use("/users", userRouters);
 
-module.exports=app;
+module.exports = app;
